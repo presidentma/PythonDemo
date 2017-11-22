df916c3ac7d069ab780d74d3f8d37abdcb9d88c8
presidentma <presidentma>
2017-11-22 17:56:23 +0800

    11:22

--- Scrapy/jianshu/jianshu/items.py
+++ Scrapy/jianshu/jianshu/items.py
@@ -12,3 +12,9 @@ class JianshuItem(scrapy.Item):
     # define the fields for your item here like:
     # name = scrapy.Field()
     author = scrapy.Field()
+    title = scrapy.Field()
+    pulish_time = scrapy.Field()
+    abstract = scrapy.Field()
+    read_number = scrapy.Field()
+    comment_number = scrapy.Field()
+    collect_number = scrapy.Field()
